35f27f1ba1ec7ed189f11e1d607fbed1e073260f
Mathew Maiseenko <maislov.alex@gmail.com>
2024-10-16 14:08:53 +0300

    styled(minimalist-select): add dark styles to component

--- src/shared/ui/minimalist-select/index.tsx
+++ src/shared/ui/minimalist-select/index.tsx
@@ -23,19 +23,15 @@ export const MinimalistSelect = ({
 	setSelected,
 }: MinimalistSelectProps) => {
 	const [inputValue, setInputValue] = useState('')
-	//const [selectedOption, setSelected] = useState('')
 	const [open, setOpen] = useState(false)
 
 	return (
 		<section className={className}>
 			<article
 				onClick={() => setOpen(!open)}
-				// className={`bg-white w-full p-2 flex items-center justify-between rounded ${
-				// 	!selected && 'text-textGray'
-				// }`}
-
-				className={`bg-inherit text-textGray w-full p-2 flex items-center justify-between rounded relative after:absolute after:left-0 after:-bottom-0.5 after:h-[1px] after:w-full after:bg-textGray after:content-['']  ${
-					selectedOption && 'text-white after:bg-white'
+				className={`bg-inherit text-textGray w-full p-2 flex items-center justify-between rounded relative after:absolute after:left-0 after:-bottom-0.5 after:h-px after:w-full after:bg-textGray after:content-['']  ${
+					selectedOption &&
+					'dark:text-white dark:after:bg-white text-lightThemeTextDarkGray after:bg-lightThemeTextDarkGray'
 				}`}
 			>
 				{selectedOption
@@ -44,24 +40,10 @@ export const MinimalistSelect = ({
 						: selectedOption
 					: message}
 				<ArrowIcon
-					styles={`transform transition-transform duration-300 rotate-0 ${
-						open ? 'rotate-180' : ''
+					styles={`transform transition-transform duration-300 rotate-0 fill-textGray ${
+						open && 'rotate-180 dark:fill-white fill-black'
 					}`}
-					fill={selectedOption.length ? 'white' : '#666666'}
 				/>
-				{/* <svg
-					width='14'
-					height='9'
-					viewBox='0 0 14 9'
-					fill={selected ? 'white' : '#666666'}
-					xmlns='http://www.w3.org/2000/svg'
-					// className={`rotate-0 ${open ? 'rotate-180' : ''}`}
-					className={`transform transition-transform duration-300 rotate-0 ${
-						open ? 'rotate-180' : ''
-					}`}
-				>
-					<path d='M13.6673 1.83333L7.00065 8.5L0.333984 1.83333L1.51732 0.649999L7.00065 6.13333L12.484 0.649999L13.6673 1.83333Z' />
-				</svg> */}
 			</article>
 			<ul
 				className={`bg-darkGray mt-2 overflow-y-auto rounded-md  ${
@@ -69,26 +51,28 @@ export const MinimalistSelect = ({
 				} `}
 			>
 				{withSearch && (
-					<section className='flex justify-between items-center px-2 py-1 sticky top-0 bg-darkGray'>
-						{/* <AiOutlineSearch size={18} className='text-gray-700' /> */}
-						{/* иконка 2 */}
-						<MagnifierIcon styles='bg-inherit min-h-full absolute right-4 top-0 w-4 h-4' />
+					<section className='flex justify-between items-center px-2 py-1 sticky top-0 dark:border-none border-2 border-lightThemeBorderGray bg-white dark:bg-darkGray'>
+						<MagnifierIcon
+							styles={`bg-inherit min-h-full absolute right-4 top-0 w-4 h-4 fill-textGray ${
+								open && 'dark:fill-white fill-black'
+							}`}
+						/>
 						<input
 							type='text'
 							value={inputValue}
 							onChange={e => setInputValue(e.target.value.toLowerCase())}
 							placeholder={searchMessage}
-							className='placeholder:text-textGray text-white p-2 outline-none bg-darkGray'
+							className='placeholder:text-textGray text-black dark:text-white p-2 outline-none bg-inherit'
 						/>
 					</section>
 				)}
 				{options.map(option => (
 					<li
 						key={option}
-						className={`p-2 text-white text-sm hover:bg-orange hover:text-black
+						className={`p-2 text-black dark:text-white text-sm hover:bg-hoverBlue dark:hover:bg-orange hover:text-black
             ${
 							option?.toLowerCase() === selectedOption?.toLowerCase() &&
-							'bg-orange text-black'
+							'bg-activeBlue dark:bg-orange text-black'
 						}
             ${
 							option?.toLowerCase().startsWith(inputValue) ? 'block' : 'hidden'
